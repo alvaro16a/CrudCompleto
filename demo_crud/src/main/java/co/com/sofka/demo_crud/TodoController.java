26a0ab1199cbfa1a6d476d536ebc404b4087fd49
Alvaro Mena <alvaro16a@gmail.com>
2022-02-23 21:49:32 -0500

    Se hizo una prueba preliminar de la interfaz, se valido que se agregaban nombres y que se asignaba de manera automatica los id

--- demo_crud/src/main/java/co/com/sofka/demo_crud/TodoController.java
+++ demo_crud/src/main/java/co/com/sofka/demo_crud/TodoController.java
@@ -3,6 +3,7 @@ package co.com.sofka.demo_crud;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class TodoController {
 
     @Autowired
